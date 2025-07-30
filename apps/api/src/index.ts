4f0fecf439c5170f36c4b775280d1b2968e6065b
Mahendradeokar <deokarmahendra424@gmail.com>
2025-07-30 23:19:39 +0530

    Initial commit

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -4,6 +4,8 @@ import { createServer } from "./server";
 const port = process.env.PORT || 5001;
 const server = createServer();
 
+server.get("/", (req, res) => res.send("WOrking"));
+
 server.listen(port, () => {
   log(`api running on ${port}`);
 });
